92fb30a78662493235f7edf06c9b40c7189a6176
ehco1996 <zh19960202@gmail.com>
2017-12-26 16:15:56 +0800

    增加流量记录的日期字段

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shadowsockesr
 ssr mod version for django-sspanel
+
+**this projecet is forked from breakwa11**
---
+++ apiconfig.py
@@ -0,0 +1,15 @@
+﻿# Config
+API_INTERFACE = 'sspanelv2' #mudbjson,sspanelv3ssr,ehcomod <谜之屋专用>
+UPDATE_TIME = 60
+SERVER_PUB_ADDR = '127.0.0.1' # mujson_mgr need this to generate ssr link
+
+#mudb
+MUDB_FILE = 'mudb.json'
+
+# Mysql
+MYSQL_CONFIG = 'usermysql.json'
+
+# API
+MUAPI_CONFIG = 'usermuapi.json'
+
+
