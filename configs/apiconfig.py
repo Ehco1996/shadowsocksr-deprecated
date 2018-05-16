ceb464a3f4dbfdc49417833316c4eccff2012544
ehco1996 <zh19960202@gmail.com>
2018-05-16 20:18:12 +0800

    add token config

--- configs/apiconfig.py
+++ configs/apiconfig.py
@@ -1,22 +1,9 @@
-﻿import base64
-# Config
-
-# API_INTERFACE = 'ehcomod'  # ehcomod <谜之屋专用> # webapi
+﻿# API_INTERFACE = 'ehcomod'  # ehcomod <谜之屋专用> # webapi
 API_INTERFACE = 'webapi'  # ehcomod <谜之屋专用> # webapi
 UPDATE_TIME = 60
 
-
 # Webapi token
-USERNAME = 'ehco'
-PORT = 2345
-try:
-    TOKEN = base64.b64encode(
-        bytes('{}+{}'.format(USERNAME, PORT), 'utf8')).decode()
-except:
-    TOKEN = base64.b64encode(
-        bytes('{}+{}'.format(USERNAME, PORT))).decode()
-
-
+TOKEN = "you own token"
 WEBAPI_URL = 'http://127.0.0.1:8000/api'
 NODE_ID = 1
 
